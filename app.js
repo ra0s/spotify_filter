85ab6c4d1112205278200e637f2abb6cf7a5da4c
SId <spideysidrao@gmail.com>
2018-07-16 15:35:40 -0700

    Added gitignore and keys

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+require('dotenv').config()
+console.log(process.env.CLIENT_ID);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
