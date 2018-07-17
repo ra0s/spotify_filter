dcbaa48cddf2b66313f932efd3a7f24a3aa0cae3
Joe Lin <JOE@Joes-MacBook-Pro.local>
2018-07-17 15:39:11 -0700

    add search function

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+// const SpotifyWebApi = require('spotify-web-api-node');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const spotify = require('./public/javascripts/methods');
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -38,4 +39,55 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 //test
+
+// var spotifyApi = new SpotifyWebApi({
+//   clientId: 'cfce71627486462793a9998a7c46c03a',
+//   clientSecret: 'b6e52ba4947b4e8ea700db9423cdba9c',
+//   // accessToken: '',
+// });
+
+// spotifyApi.clientCredentialsGrant().then(
+//   function (data) {
+//     console.log('The access token expires in ' + data.body['expires_in']);
+//     console.log('The access token is ' + data.body['access_token']);
+
+//     // Save the access token so that it's used in future calls
+//     spotifyApi.setAccessToken(data.body['access_token']);
+//     // this.accessToken = data.body['access_token'];
+//   },
+//   function (err) {
+//     console.log(
+//       'Something went wrong when retrieving an access token',
+//       err.message
+//     );
+//   }
+// ).then(function () {
+//   spotifyApi.getArtistAlbums(
+//     '43ZHCT0cAZBISjO8DG9PnE',
+//     { limit: 10, offset: 20 },
+//     function (err, data) {
+//       if (err) {
+//         console.error('Something went wrong!');
+//       } else {
+//         console.log(data.body);
+//       }
+//     }
+//   );
+// })
+
+// spotifyApi.getArtistAlbums(
+//   '43ZHCT0cAZBISjO8DG9PnE',
+//   { limit: 10, offset: 20 },
+//   function (err, data) {
+//     if (err) {
+//       console.error('Something went wrong!');
+//     } else {
+//       console.log(data.body);
+//     }
+//   }
+// );
+
+// spotify.data.initCredential();
+
+
 module.exports = app;
