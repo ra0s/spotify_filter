c8be3f80f7387e2f9eeac69e9e0ac6a9e8f267cf
SId <spideysidrao@gmail.com>
2018-07-17 15:10:17 -0700

    Small changes

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config()
-console.log(process.env.CLIENT_ID);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
