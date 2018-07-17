830457e5ee8cd843797d9ea950f4240cb365e694
SId <spideysidrao@gmail.com>
2018-07-17 16:09:45 -0700

    Fixed crashes

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const usersRouter = require('./routes/users');
 const spotify = require('./public/javascripts/methods');
 const app = express();
 require('dotenv').config()
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
