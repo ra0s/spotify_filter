567aba4a301a00398345d5ba4e77c8e95fcf5259
magical-mishka <mishka.14@gmail.com>
2018-07-17 14:57:43 -0700

    Experimenting
    Commiting to get Materialize from David.

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+
+
+  function myFunction() {
+      document.getElementById("demo").style.color = "red";
+  }
+
+  function changeImage(){
+    document.getElementById("cover-art").style.opacity="0.2";
+    document.getElementById("cover-art").transition="200ms";
+
+  }
