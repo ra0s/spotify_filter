77ef2d57140515ce1f459c215f917813d84e8ac7
magical-mishka <mishka.14@gmail.com>
2018-07-18 15:02:11 -0700

    Results page works

--- public/javascripts/methods.js
+++ public/javascripts/methods.js
@@ -25,11 +25,7 @@ spotifyApi.initCredential = function() {
 
 spotifyApi.searchCategory = (category) => {
     const tracklist = [];
-<<<<<<< HEAD
     return spotifyApi.searchTracks('genre:' + category, {limit: 5})
-=======
-    return spotifyApi.searchTracks('genre:' + category, {limit: 50})
->>>>>>> sid
         .then((data) => {
             console.log('Search tracks by ' + data + ' in the artist name', data.body.tracks.items[0].album.name);
             data.body.tracks.items.forEach( (data)=> {
--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,8 @@ router.get('/categories', function(req, res){
   console.log(req.query.categories)
   spotify.spotifyApi.searchCategory(req.query.categories)
   .then(function(result) {
-<<<<<<< HEAD
-    console.log(result);
+    console.log("THIS IS THE TRACKLIST\n" + result);
     res.render('results', {tracklist: result} )
-=======
-    console.log(result); 
-    res.render('/results');
->>>>>>> sid
   } )
 })
 
