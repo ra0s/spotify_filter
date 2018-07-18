a05467966505bb86cd5b0d928f0aa7a8368aa72b
SId <spideysidrao@gmail.com>
2018-07-18 11:33:19 -0700

    Tracklist return working

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,20 @@ const spotify = require('../public/javascripts/methods');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  spotify.data.initCredential();
+  spotify.spotifyApi.initCredential();
   res.render('index');
 });
 
+router.get('/results', function(req, res, next){
+  res.render('results');
+});
 router.get('/categories', function(req, res){
   console.log(req.query.categories)
-  // req.query.categories;
-  spotify.data.searchCategory(req.query.categories);
-  res.redirect('/')
+  spotify.spotifyApi.searchCategory(req.query.categories)
+  .then(function(result) {
+    console.log(result); 
+    res.redirect('/')
+  } )
 })
 
 module.exports = router;
