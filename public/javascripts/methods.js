b40ffd6a913cf210d580cb51ce4853aee0a33f12
magical-mishka <mishka.14@gmail.com>
2018-07-19 11:06:42 -0700

    Added Navbar

--- public/javascripts/methods.js
+++ public/javascripts/methods.js
@@ -25,17 +25,41 @@ spotifyApi.initCredential = function() {
 
 spotifyApi.searchCategory = (category) => {
     const tracklist = [];
-    return spotifyApi.searchTracks('genre:' + category, {limit: 16})
+    return spotifyApi.searchTracks('genre:' + category, {limit: 50})
         .then((data) => {
             console.log('Search tracks by ' + data + ' in the artist name', data.body.tracks.items[0].album.name);
             data.body.tracks.items.forEach( (data)=> {
                 tracklist.push(new Track(data.album.name, data.popularity, data.artists[0].name, data.name, data.uri))
             })
-            return tracklist;
+            return shuffle(tracklist);
         })
         .catch((err) =>{
             console.log(err);
         });
 }
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    const tracklist = [];
+    for( let i = 0; i < 20; i++)
+    {
+        tracklist.push(array[i]);
+    }
+
+    return tracklist;
+ }
 
 exports.spotifyApi = spotifyApi;
