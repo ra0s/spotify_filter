3155b540d6db5021e89fc5a23986a502c22180ec
magical-mishka <mishka.14@gmail.com>
2018-07-20 14:32:42 -0700

    Final version
    Has a working sidebar which can remove items upon clicking item.

--- public/javascripts/methods.js
+++ public/javascripts/methods.js
@@ -40,6 +40,7 @@ function shuffle(array) {
    return array;
 }
 
+
 spotifyApi.findTracks = (category, pop) => {
     const tracklist = [];
     let count = 0;
@@ -49,7 +50,7 @@ spotifyApi.findTracks = (category, pop) => {
 
     return recursive(tracklist, category, os, pop, count);
 }
-   
+
 function recursive(arr, category, os, pop, count){
     return spotifyApi.searchTracks('genre:' + category, { limit: 50, offset: os, time_range: 'medium_term'})
     .then((data) => {
@@ -72,5 +73,28 @@ function recursive(arr, category, os, pop, count){
     .catch( (err) => { console.log(err)});
 }
 
-exports.spotifyApi = spotifyApi;
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
 
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    const tracklist = [];
+    for( let i = 0; i < 12; i++)
+    {
+        tracklist.push(array[i]);
+    }
+
+    return tracklist;
+ }
+
+exports.spotifyApi = spotifyApi;
--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,9 @@
-console.log('cardTop');
 
 var allAddButtons = document.getElementsByClassName("btn-floating");
+
+var artistList = [];
+var songList = [];
+var albumList = [];
 
 var addSongToList = function() {
   var musicList = document.getElementById('musiclist');
@@ -8,21 +11,55 @@ var addSongToList = function() {
   var songName = this.getAttribute('name');
   var artistName = this.getAttribute('artist');
   var albumName = this.getAttribute('album');
-
+  var uri = this.getAttribute('uri');
   console.log(songName);
+  console.log(uri);
 
   var htmlToInsert =
-    '<li class="collection-item avatar">\n' +
+    '<a class="collection-item avatar song-item" onclick="removeItem()">\n' +
     '<img src="images/yuna.jpg" alt="" class="circle">\n' +
     '<i class="material-icons circle">music_note</i>\n' +
     '<span class="title"><b>'+ songName+'</b></span>\n' +
     '<p>'+artistName+ '<br></p>\n' +
     '<p class="grey-text">'+albumName+'</p>\n' +
-    '</li>';
+    '<i class="secondary-content material-icons" id= "button-remove">remove_circle</i>\n'+
+    '</a>';
 
   musicList.insertAdjacentHTML('beforeend', htmlToInsert);
+
+  var descriptionItem = document.getElementById('description-list');
+  descriptionItem.remove();
+
+  artistList.push(artistName);
+  songList.push(songName);
+  albumList.push(albumName);
+
+  console.log (artistList);
+  console.log(songName);
+  console.log(albumName);
+
+
 };
 
 for (var i = 0; i < allAddButtons.length; i++) {
   allAddButtons[i].addEventListener('click', addSongToList, false);
 }
+
+
+var songItems = document.getElementsByClassName("song-item");
+
+// for (var i = 0; i < songItems.length; i++) {
+//   console.log(i);
+//   console.log(songItems[i]);
+//   songItems[i].addEventListener('click', removeItem(), false);
+//   }
+
+  function removeItem(){
+
+// songitems[i].remove();
+    // var itemToRemove = songItems[i];
+    // itemToRemove.remove();
+
+      (event.currentTarget).remove();
+
+  }
