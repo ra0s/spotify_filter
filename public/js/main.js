7330347db4707a6fe56e9e445633a5eeff434bc6
Joe Lin <37271083+JoeJLin@users.noreply.github.com>
2018-07-20 14:41:21 -0700

    Changes to results page (#19)
    * Design with a grid of 16 items.
    
    Search results show 16 cards only in a wrapped format.
    
    * Added Navbar
    
    * Added floating buttons to cards
    
    * Added fixed working sidebar to create a playlist
    
    The sidebar can add song information when the plus button is clicked on any card.
    
    * Final version
    
    Has a working sidebar which can remove items upon clicking item.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
-console.log('cardTop');
-
 var allAddButtons = document.getElementsByClassName("btn-floating");
+
+var artistList = [];
+var songList = [];
+var albumList = [];
 
 var addSongToList = function() {
   var musicList = document.getElementById('musiclist');
@@ -8,21 +10,36 @@ var addSongToList = function() {
   var songName = this.getAttribute('name');
   var artistName = this.getAttribute('artist');
   var albumName = this.getAttribute('album');
-
+  var uri = this.getAttribute('uri');
   console.log(songName);
+  console.log(uri);
 
   var htmlToInsert =
-    '<li class="collection-item avatar">\n' +
+    '<a class="collection-item avatar song-item" onclick="removeItem()">\n' +
     '<img src="images/yuna.jpg" alt="" class="circle">\n' +
     '<i class="material-icons circle">music_note</i>\n' +
     '<span class="title"><b>'+ songName+'</b></span>\n' +
     '<p>'+artistName+ '<br></p>\n' +
     '<p class="grey-text">'+albumName+'</p>\n' +
-    '</li>';
+    '<i class="secondary-content material-icons" id= "button-remove">remove_circle</i>\n'+
+    '</a>';
 
   musicList.insertAdjacentHTML('beforeend', htmlToInsert);
+
+  var descriptionItem = document.getElementById('description-list');
+  descriptionItem.remove();
+
+  artistList.push(artistName);
+  songList.push(songName);
+  albumList.push(albumName);
+
+  console.log (artistList);
+  console.log(songName);
+  console.log(albumName);
+
 };
 
 for (var i = 0; i < allAddButtons.length; i++) {
   allAddButtons[i].addEventListener('click', addSongToList, false);
 }
+
