7fd8e45b9cdf1e6d64bfc1bbb6ff84a70582dacd
Sid Rao <spideysidrao@gmail.com>
2018-07-21 16:06:38 -0700

    remove duplicate function

--- public/js/methods.js
+++ public/js/methods.js
@@ -22,25 +22,6 @@ spotifyApi.initCredential = function() {
            console.log(err)
        })
 }
-function shuffle(array) {
-   var currentIndex = array.length, temporaryValue, randomIndex;
-
-   // While there remain elements to shuffle...
-   while (0 !== currentIndex) {
-
-       // Pick a remaining element...
-       randomIndex = Math.floor(Math.random() * currentIndex);
-       currentIndex -= 1;
-
-       // And swap it with the current element.
-       temporaryValue = array[currentIndex];
-       array[currentIndex] = array[randomIndex];
-       array[randomIndex] = temporaryValue;
-   }
-
-   return array;
-}
-
 
 spotifyApi.findTracks = (category, pop) => {
     const tracklist = [];
