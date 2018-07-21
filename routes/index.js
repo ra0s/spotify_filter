538d3780fcfa7926b640a6919cc941cafe870162
SId <spideysidrao@gmail.com>
2018-07-21 14:14:11 -0700

    Cleaned up code organization, deleted unnecessary files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const spotify = require('../public/javascripts/methods');
+const spotify = require('../public/js/methods');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
